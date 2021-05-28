c9878e93c79ffa197db9fba6ec194b4146677e97
stonel9687 <douglas.reyesquintero09@gmail.com>
2021-05-27 22:42:25 -0400

    feat: add function state players

--- src/modules/home/pages/home.js
+++ src/modules/home/pages/home.js
@@ -1,20 +1,10 @@
-import { useState } from 'react'
+import { useContext } from 'react'
 import { Link } from "react-router-dom"
+import { Context } from '../../../store/appContex'
 
 const Home = () => {
-    const [players, setPlayers] = useState({
-        playerOne: {
-            name: '',
-            simbolSelected: ''
-        }, playerTwo: {
-            name: '',
-            simbolSelected: ''
-        }
-    })
 
-    const handleChange = (e) => {
-        setPlayers({ ...players, [e.target.name]: { ...players[e.target.name], name: e.target.value } })
-    }
+    const { store, actions } = useContext(Context)
 
     return (
         <>
@@ -29,20 +19,20 @@ const Home = () => {
                                 <form>
                                     <div className='d-flex'>
                                         <div className='w-80 player-box'>
-                                            <input required type="text" className="form-control " placeholder="PLAYER 1" aria-label="Recipient's username" name='playerOne' value={players.playerOne.name} onChange={action.handleChange} />
+                                            <input required type="text" className="form-control " placeholder="PLAYER 1" aria-label="Recipient's username" name='playerOne' value={store.playerOne.name} onChange={actions.handleChange} />
                                         </div>
                                         <div>
-                                            <button type="button" class="btn btn-outline-danger">X</button>
-                                            <button type="button" class="btn btn-outline-dark">O</button>
+                                            <button type="button" onClick={() => { actions.handleSelect('X') }} className="btn btn-outline-danger">X</button>
+                                            <button type="button" onClick={() => { actions.handleSelect('O') }} className="btn btn-outline-dark">O</button>
                                         </div>
                                     </div>
                                     <div className='d-flex'>
                                         <div className='w-80 player-box'>
-                                            <input required type="text" className="form-control " placeholder="PLAYER 2" aria-label="Recipient's username" name='playerTwo' value={players.playerTwo.name} onChange={handleChange} />
+                                            <input required type="text" className="form-control " placeholder="PLAYER 2" aria-label="Recipient's username" name='playerTwo' value={store.playerTwo.name} onChange={actions.handleChange} />
                                         </div>
                                         <div>
-                                            <button type="button" class="btn btn-outline-danger">X</button>
-                                            <button type="button" class="btn btn-outline-dark">O</button>
+                                            <button type="button" onClick={() => { actions.handleSelect('O') }} className="btn btn-outline-danger">X</button>
+                                            <button type="button" onClick={() => { actions.handleSelect('X') }} className="btn btn-outline-dark">O</button>
                                         </div>
                                     </div>
                                 </form>
--- src/store/flux.js
+++ src/store/flux.js
@@ -1,9 +1,45 @@
 const getState = ({ getStore, setStore, getActions }) => {
     return {
         store: {
-           player:'douglas'
+            playerOne: {
+                name: '',
+                simbolSelected: ''
+            },
+            playerTwo: {
+                name: '',
+                simbolSelected: ''
+            }
         },
         actions: {
+            handleChange: (e) => {
+                const store = getStore()
+                const player = { ...store[e.target.name], name: e.target.value }
+                console.log(player)
+
+                setStore({
+                    [e.target.name]: player
+                })
+            },
+
+            handleSelect: (symbol) => {
+                const store = getStore()
+                let symbolEdited = undefined
+                let symbolEditedTwo = undefined
+                if (symbol === 'X') {
+                    symbolEdited = { ...store.playerOne, simbolSelected: 'x' }
+                    symbolEditedTwo = { ...store.playerTwo, simbolSelected: 'o' }
+                } else {
+                    symbolEditedTwo = { ...store.playerTwo, simbolSelected: 'x' }
+                    symbolEdited = { ...store.playerOne, simbolSelected: 'o' }
+                }
+                setStore({
+                    playerOne: symbolEdited,
+                    playerTwo: symbolEditedTwo
+                })
+            },
+
+
+
 
         }
     }
